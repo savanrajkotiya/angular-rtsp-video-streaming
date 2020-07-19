b3271a7c14e43ac955c25f439520aba2ff9fceec
Savan Rajkotiya <savanrajkotiya2396@gmail.com>
2020-07-19 16:00:04 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angularRTSP-Stremedian';
+
+export class AppComponent implements OnInit {
+  mediaElement: HTMLElement;
+  
+  playerOptions = { // RTSP player options
+    socket: "wss://streamedian.com/ws/", // --> Proxy socket URL for connection
+    bufferDuration: 30,
+    errorHandler: (error) => {
+      console.log(error);
+    },
+    infoHandler: (info) => { console.log('Info', info) }
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+    this.mediaElement = document.getElementById('test_video'); // Get HTML media element 
+    if (window['Streamedian']) { // check streamedian in window object
+      let registerPlayer = window['Streamedian'].player(this.mediaElement, this.playerOptions)
+    }
+  }
+
 }
